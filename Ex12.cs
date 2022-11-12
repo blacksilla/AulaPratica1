077c86ea0a01237d056b08e67e893652beb6e2ea
blacksilla <afonsosilva1@ua.pt>
2022-11-12 23:56:55 +0000

    Add files via upload

---
+++ Ex12.cs
@@ -0,0 +1,19 @@
+﻿namespace AulaPratrica1.Ex12
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Introduza um número");
+            int num1 = int.Parse(Console.ReadLine());
+            if (num1 % 2 == 0)
+            {
+                Console.WriteLine("O número introduzido é par");
+            }
+            else
+            {
+                Console.WriteLine("O número introduzido é impar");
+            }
+        }
+    }
+}
